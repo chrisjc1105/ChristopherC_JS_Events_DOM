92d21ec7e47adf19f9207be3ce9081276da8bfd4
Christopher <chriscamachwork@gmail.com>
2025-04-02 22:34:43 -0400

    second draft

--- script.js
+++ script.js
@@ -20,19 +20,29 @@ clickMeButton.addEventListener("click", function (event) {
     clickMeButton.innerText = "You clicked me!";
 })
 
+// if it were to be document.addEventListener({...})
+// then clicking on the document itself would change the text,
+// not the button specifically (which is what we really want).
+
 let colorBtn = document.getElementById("randomColorBtn");
 
+colorBtn.style.borderRadius = "20px";
+
 function Random() {
+
+    // complex version vvv
     let redValue = Math.floor(Math.random() * 255);
     let greenValue = Math.floor(Math.random() * 255);
     let blueValue = Math.floor(Math.random() * 255);
 
-    colorBtn.style.backgroundColor = 'rgb(' + redValue + ',' + greenValue + ',' + blueValue + ')';
+    document.body.style.backgroundColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
 
+    alert("Changed color!");
     console.log(redValue);
     console.log(greenValue);
     console.log(blueValue);
-    console.log(colorBtn.style.backgroundColor);
+    console.log(document.body.style.backgroundColor);
+
     // simpler version vvv
 
     // switch (colorValue) {
@@ -60,6 +70,21 @@ function Random() {
     // }
 }
 
-// if it were to be document.addEventListener({...})
-// then clicking on the document itself would change the text,
-// not the button specifically (which is what we really want).
+let modeBtn = document.getElementById("modeBtn");
+
+modeBtn.addEventListener("click", function(event) {
+    if (document.body.className == "light-mode") {
+        modeBtn.innerText = "Click me to toggle light mode"
+        document.body.classList.remove("light-mode");
+        document.body.classList.toggle("dark-mode");
+    } else if (document.body.className == "dark-mode") {
+        modeBtn.innerText = "Click me to toggle dark mode"
+        document.body.classList.remove("dark-mode");
+        document.body.classList.toggle("light-mode");
+    } else {
+        console.log('error!');
+    }
+    console.log(document.body.className);
+    console.log(modeBtn.className);
+    console.log(event);
+}); 
